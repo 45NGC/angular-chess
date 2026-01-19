db2556b2514e64319db4a52e5298c4b7f17f4fa9
45NGC <nicolasnerfo@gmail.com>
2026-01-19 20:59:26 +0100

    added piece images

--- src/app/ui/game/game.component.ts
+++ src/app/ui/game/game.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { GameState } from '../../core/rules/game-state';
 import { CommonModule } from '@angular/common';
+import { loadFEN } from '../../core/board/fen';
+import { Board } from '../../core/board/board';
 
 @Component({
 	selector: 'app-game',
@@ -10,12 +12,24 @@ import { CommonModule } from '@angular/common';
 	styleUrls: ['./game.component.css']
 })
 export class GameComponent {
-	state = new GameState();
+	state!: GameState;
+
+	constructor() {
+		const board = new Board();
+		loadFEN(board, 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
+		this.state = new GameState(board);
+	}
 
 	ranks = Array.from({ length: 8 }, (_, i) => 7 - i);
 	files = Array.from({ length: 8 }, (_, i) => i);
 
-	isLightSquare(rank: number, file: number): boolean {
+	isDarkSquare(rank: number, file: number): boolean {
 		return (rank + file) % 2 === 0;
+	}
+
+	pieceToImage(piece: any): string | null {
+		if (!piece) return null;
+
+		return `../../assets/pieces/${piece.color}-${piece.type}.png`;
 	}
 }
